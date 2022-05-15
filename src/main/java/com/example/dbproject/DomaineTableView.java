215d2dadeab5a543d1d48630020e2cdf642cf05d
feriel214 <fzarrouki1999@gmail.com>
2022-05-15 13:50:21 +0100

    navigation

--- src/main/java/com/example/dbproject/DomaineTableView.java
+++ src/main/java/com/example/dbproject/DomaineTableView.java
@@ -1,41 +1,55 @@
 package com.example.dbproject;
 
 import com.example.dbproject.models.Domaine;
-import com.example.dbproject.models.Formateur;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.DragEvent;
+import org.w3c.dom.events.MouseEvent;
 
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ResourceBundle;
+
+import static java.lang.Integer.parseInt;
+
 public class DomaineTableView implements Initializable  {
 
 
 
     DatabaseConnection connect= new DatabaseConnection();
     Connection connectDB = connect.getConnection();
+
     @FXML
-    private Button adddomaine;
+    private Button AddDomaineBtn;
     @FXML
     private TableView<Domaine> domainesTable;
     @FXML
     private TableColumn<Domaine, Integer> idCol;
     @FXML
     private TableColumn<Domaine,String> libCol;
+    @FXML
+    private Label domaineaddmsg;
+    @FXML
+    private TextField IdDom;
+    @FXML
+    private Button editBtn;
+    @FXML
+    private TextField domaineLibelle;
     ObservableList<Domaine> domainesList = FXCollections.observableArrayList();
     Domaine domain = null ;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        idCol.setCellValueFactory(new PropertyValueFactory<>("code_domaine"));
+        libCol.setCellValueFactory(new PropertyValueFactory<>("libelle"));
+        editBtn.setVisible(false);
         loadData();
     }
 
@@ -72,9 +86,63 @@ public class DomaineTableView implements Initializable  {
 
 
     @FXML
-    void Adddomaine(ActionEvent event) {
+    void AddDomaine(ActionEvent event) {
+        this.PostDomaine();
+    }
+    public void PostDomaine(){
+        DatabaseConnection connect= new DatabaseConnection();
+        Connection connectDB = connect.getConnection();
+        Domaine domaine=new Domaine(domaineLibelle.getText());
+
+        String insertDomaine="INSERT INTO domaine (libelle) VALUES ('"+domaine.getLibelle()+"')";
+
+        try{
+            Statement stmt=connectDB.createStatement();
+            stmt.executeUpdate(insertDomaine);
+            Refresh();
+            this.refershTable();
+            //domaineaddmsg.setText("Domaine Ajouter Avec succées!");
+        }catch(Exception e ){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+
+
+    public void Refresh(){
+        this.IdDom.setText("");
+        this.domaineLibelle.setText("");
 
     }
 
 
+    public void rowClicked(javafx.scene.input.MouseEvent mouseEvent) {
+        Domaine clickedom=domainesTable.getSelectionModel().getSelectedItem();
+        this.domaineLibelle.setText(String.valueOf(clickedom.getLibelle()));
+        this.IdDom.setText((clickedom.getCode_domaine()).toString());
+        AddDomaineBtn.setVisible(true);
+        editBtn.setVisible(true);
+
+    }
+
+    @FXML
+    void editDomaine(ActionEvent event) {
+
+        String updateDomaine="UPDATE domaine SET `libelle`='"+domaineLibelle.getText()+"' WHERE `code_domaine`= "+parseInt(IdDom.getText());
+
+        try{
+            Statement stmt=connectDB.createStatement();
+            stmt.executeUpdate(updateDomaine);
+            Refresh();
+            this.refershTable();
+            //domaineaddmsg.setText("Domaine Modifié Avec succées!");
+            this.editBtn.setVisible(false);
+            this.AddDomaineBtn.setVisible(true);
+        }catch(Exception e ){
+            e.printStackTrace();
+            e.getCause();
+        }
+
+    }
 }
